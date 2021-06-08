831fb1f1c5b448c3a4d2f406dfd8c243902a514a
Semil Khedawala <semilkhedawala@gmail>
2021-06-08 20:53:57 +0530

    anonymous sign_in completed

---
+++ lib/app/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class HomePage extends StatelessWidget {
+  final VoidCallback onSignOut;
+  final AuthBase auth;
+  const HomePage({Key key, @required this.onSignOut, @required this.auth})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future<void> _signOut() async {
+      try {
+        await auth.signOut();
+        onSignOut();
+      } catch (e) {
+        print(e.toString());
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        actions: [
+          TextButton(
+              onPressed: _signOut,
+              child: Text(
+                "Logout",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/landing_page.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home_page.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class LandingPage extends StatefulWidget {
+  final AuthBase auth;
+
+  const LandingPage({Key key, @required this.auth}) : super(key: key);
+
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  void initState() {
+    super.initState();
+    _updateUser(widget.auth.currentUser);
+  }
+
+  User _user;
+  void _updateUser(User user) {
+    setState(() {
+      _user = user;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_user == null) {
+      return SignInPage(
+        auth: widget.auth,
+        onSignIn: _updateUser,
+      );
+    }
+    return HomePage(
+      auth: widget.auth,
+      onSignOut: () => _updateUser(null),
+    );
+  }
+}
